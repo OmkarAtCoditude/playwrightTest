823a02c95c56e4733346f4d87f91794d3201e4fa
saurabhkadamcaredge <saurabh.k@caredge.com>
2024-06-24 13:08:45 +0530

    playwright config set for allure report

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, devices } from "@playwright/test";
+import { testPlanFilter } from "allure-playwright/testplan";
 
 /**
  * Read environment variables from file.
@@ -21,7 +22,9 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? "blob" : "html",
+  // reporter: process.env.CI ? "blob" : "html",
+  grep: testPlanFilter(),
+  reporter: [["line"], ["allure-playwright"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
